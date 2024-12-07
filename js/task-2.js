82f2e2ecdf3ba025aeb5e4538c8d8921c8ef0ddb
Serhii Krasnozhon <zhonik92@gmail.com>
2024-12-07 17:13:26 +0200

    create homework

---
+++ js/task-2.js
@@ -0,0 +1,16 @@
+function formatMessage(message, maxLength) {
+  let shortMessage;
+  if (message.length <= maxLength) {
+    return message;
+  } else if (message.length > maxLength) {
+    shortMessage = message.slice(0, maxLength) + '...';
+    return shortMessage;
+  }
+}
+
+console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
+console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
+console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
+console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
